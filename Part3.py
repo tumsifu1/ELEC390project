6b27cc7f09ff222beaa2addd64073de4fd490ee6
aaron moise <19ajgm@queensu.ca>
2023-03-27 00:50:18 -0400

    part 3 is done

---
+++ Part3.py
@@ -0,0 +1,90 @@
+import h5py
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def read_data(activity):
+    with h5py.File('./accelerometer_data.h5', 'r') as f:
+        member_walking_data = f['MP/walking'][:]
+        member_jumping_data = f['MP/jumping'][:]
+    if activity == 'walking':
+        return member_walking_data
+    elif activity == 'jumping':
+        return member_jumping_data
+    else:
+        raise ValueError('Invalid activity value')
+
+def plot_data(ax, data, title, color, label):
+    time = data[:, 0]
+    acceleration_x = data[:, 1]
+    acceleration_y = data[:, 2]
+    acceleration_z = data[:, 3]
+    ax.plot(time, np.sqrt(acceleration_x**2 + acceleration_y**2 + acceleration_z**2), color=color, label=label)
+    ax.set_title(title)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Acceleration Magnitude')
+    ax.legend()
+
+# Read the data
+jumping_data = read_data('jumping')
+walking_data = read_data('walking')
+
+# Time series plot of acceleration magnitude for walking and jumping
+fig, ax = plt.subplots(figsize=(12, 8))
+plot_data(ax, walking_data, 'Acceleration Magnitude vs Time', 'blue', 'Walking')
+plot_data(ax, jumping_data, 'Acceleration Magnitude vs Time', 'red', 'Jumping')
+fig.tight_layout()
+plt.show()
+
+# Histogram of acceleration magnitudes for walking and jumping
+fig, ax = plt.subplots(figsize=(12, 8))
+ax.hist(np.linalg.norm(walking_data[:, 1:4], axis=1), bins=30, alpha=0.5, color='blue', label='Walking')
+ax.hist(np.linalg.norm(jumping_data[:, 1:4], axis=1), bins=30, alpha=0.5, color='red', label='Jumping')
+ax.set_title('Histogram of Acceleration Magnitudes')
+ax.set_xlabel('Acceleration Magnitude')
+ax.set_ylabel('Frequency')
+ax.legend()
+fig.tight_layout()
+plt.show()
+
+# Scatter plot of x vs y, x vs z, and y vs z for both walking and jumping data
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(18, 6))
+axes[0].scatter(walking_data[:, 1], walking_data[:, 2], color='blue', label='Walking', alpha=0.5)
+axes[0].scatter(jumping_data[:, 1], jumping_data[:, 2], color='red', label='Jumping', alpha=0.5)
+axes[0].set_title('Acceleration Y vs Acceleration X')
+axes[0].set_xlabel('Acceleration X')
+axes[0].set_ylabel('Acceleration Y')
+axes[0].legend()
+
+axes[1].scatter(walking_data[:, 1], walking_data[:, 3], color='blue', label='Walking', alpha=0.5)
+axes[1].scatter(jumping_data[:, 1], jumping_data[:, 3], color='red', label='Jumping', alpha=0.5)
+axes[1].set_title('Acceleration Z vs Acceleration X')
+axes[1].set_xlabel('Acceleration X')
+axes[1].set_ylabel('Acceleration Z')
+axes[1].legend()
+
+axes[2].scatter(walking_data[:, 2], walking_data[:, 3], color='blue', label='Walking', alpha=0.5)
+axes[2].scatter(jumping_data[:, 2], jumping_data[:, 3], color='red', label='Jumping', alpha=0.5)
+axes[2].set_title('Acceleration Z vs Acceleration Y')
+axes[2].set_xlabel('Acceleration Y')
+axes[2].set_ylabel('Acceleration Z')
+axes[2].legend()
+
+fig.tight_layout()
+plt.show()
+
+#3D Scatter plot of x, y, and z accelerations for both walking and jumping dat
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(walking_data[:, 1], walking_data[:, 2], walking_data[:, 3], color='blue', label='Walking', alpha=0.5)
+ax.scatter(jumping_data[:, 1], jumping_data[:, 2], jumping_data[:, 3], color='red', label='Jumping', alpha=0.5)
+
+ax.set_title('3D Scatter Plot of Acceleration')
+ax.set_xlabel('Acceleration X')
+ax.set_ylabel('Acceleration Y')
+ax.set_zlabel('Acceleration Z')
+ax.legend()
+
+plt.show()
