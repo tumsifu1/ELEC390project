e6a9ddc87d5f6b354f7b266c7f61a289abe120e3
MPatchell <79283868+MPatchell@users.noreply.github.com>
2023-04-11 14:22:12 -0400

    Close UI after submit

--- bonus.py
+++ bonus.py
@@ -8,11 +8,10 @@ import time
 from scipy.stats import skew
 import joblib
 import tkinter as tk
-from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
 
-#global var
+# global var
 driver = None
 webdriver_path = None
 ip_address = None
@@ -20,6 +19,7 @@ ip_address = None
 predicted_activity = None
 
 clfCombined = joblib.load('classifier.joblib')
+
 
 def feature_extract(window_data):
     # Compute the features
@@ -40,13 +40,22 @@ def feature_extract(window_data):
     return features
 
 
-
-
 def process_data_and_predict():
     global predicted_activity
+
+    # Close the current UI
+    window.destroy()
+
+    # create new UI with predicted activity
+    new_root = tk.Tk()
+    prediction_label = tk.Label(new_root, text="")
+    prediction_label.pack()
+    prediction_label.config(text="Waiting ...")
+    new_root.update()
+
     window_size = 50
     iterations = 15
-    time.sleep(5)
+    time.sleep(10)
     for n in range(iterations):
         # Create an empty array with shape (1, 100, 4)
         x_data_array = np.zeros(window_size)
@@ -63,12 +72,15 @@ def process_data_and_predict():
                     ec.presence_of_element_located((By.ID, "element6"))
                 )
 
-                # Collect data from elements 6, 7, 8, 9
+                # Collect data from elements 6, 7, 8
                 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-                value_x = float(soup.find('div', {'id': 'element6'}).find('span', {'class': 'valueNumber'}).text.strip())
-                value_y = float(soup.find('div', {'id': 'element7'}).find('span', {'class': 'valueNumber'}).text.strip())
-                value_z = float(soup.find('div', {'id': 'element8'}).find('span', {'class': 'valueNumber'}).text.strip())
+                value_x = float(
+                    soup.find('div', {'id': 'element6'}).find('span', {'class': 'valueNumber'}).text.strip())
+                value_y = float(
+                    soup.find('div', {'id': 'element7'}).find('span', {'class': 'valueNumber'}).text.strip())
+                value_z = float(
+                    soup.find('div', {'id': 'element8'}).find('span', {'class': 'valueNumber'}).text.strip())
                 value_total = np.sqrt(value_x ** 2 + value_y ** 2 + value_z ** 2)
 
                 # Convert values to floats and store in data_array
@@ -99,18 +111,23 @@ def process_data_and_predict():
 
         predicted_activity = Y_predicted[0]
         print(Y_predicted)
-        window.after(0, update_activity)
-        
-        #change_window_color(Y_predicted[0])
-        
-#driver = webdriver.Chrome('C:\\Users\\mjpat\\Downloads\\chromedriver_win32\\chromedriver.exe')
-#driver.get('http://192.168.2.24/')
 
-#functions for UI
+        # update the UI window with the predicted activity
+        if predicted_activity == 0:
+            prediction_label.config(text="Walking")
+        elif predicted_activity == 1:
+            prediction_label.config(text="Jumping")
+
+        # update the UI window
+        new_root.update()
+
+
+# functions for UI
 def get_webdriver_path():
     global webdriver_path
     webdriver_path = filedialog.askopenfilename()
     webdriver_path_label.config(text=webdriver_path)
+
 
 def get_ip_address():
     global ip_address
@@ -119,35 +136,32 @@ def get_ip_address():
         messagebox.showwarning("Warning", "Please enter a valid IP address.")
 
 def submit():
-    global driver, webdriver_path, ip_address
+    global driver, webdriver_path, ip_address, predicted_activity
     if webdriver_path and ip_address:
         driver = webdriver.Chrome(webdriver_path)
         driver.get(f'http://{ip_address}/')
         process_data_and_predict()
+
+        # Close the UI after the function is called
+        window.destroy()
+
     else:
         messagebox.showwarning("Warning", "Please select a web driver and enter a valid IP address before submitting.")
+
 
 def show_instructions():
     instructions = """1. Open the Phyphox app
 2. Click on "Acceleration without G"
-3. Activate the "Access with distance" option by clicking the three buttons in the top right corner of the Phyphox interface
-4. Input the URL provided by Phyphox into the UI text box and go to it on Chrome
-5. Input the location of the web driver into the UI text box"""
-
+3. Clicking the button with threes in the top right corner of the Phyphox interface and select 'Enable Remote access'
+4. Input the location of your Chrome web driver into the UI text box
+4. Input the numbers from the URL provided by Phyphox into the UI text box and click the 'Enter' button
+5. Click submit
+6. Navigate to the 'Mutli' window
+7. Look on your screen to view the activity classification (there may be an initial delay)"""
     messagebox.showinfo("Instructions", instructions)
 
-
-def update_activity(activity):
-    global predicted_activity
-    if activity == 1:
-        activity_icon_label.config(image=activity_icon_walking)
-    else:
-        activity_icon_label.config(image=activity_icon_jumping)
-
-#UI
+# UI
 window = tk.Tk()
-activity_icon_walking = tk.PhotoImage(file='walking.png')
-activity_icon_jumping = tk.PhotoImage(file='jumping.png')
 window.configure(bg='black')
 window.title('Acceleration Classifier')
 
@@ -155,7 +169,8 @@ webdriver_path_label = tk.Label(window, width=40, text="", anchor='w', bg='white
 webdriver_path_label.pack()
 webdriver_path_label.place(x=100, y=30)
 
-webdriver_path_button = tk.Button(window, text="Select Webdriver", command=get_webdriver_path, bg='white', fg='black', font=('Arial', 10, 'bold'), width=20)
+webdriver_path_button = tk.Button(window, text="Select Webdriver", command=get_webdriver_path, bg='white', fg='black',
+                                  font=('Arial', 10, 'bold'), width=20)
 webdriver_path_button.pack()
 webdriver_path_button.place(x=430, y=30)
 
@@ -163,23 +178,20 @@ ip_address_entry = tk.Entry(window, width=50)
 ip_address_entry.pack()
 ip_address_entry.place(x=100, y=70)
 
-ip_address_button = tk.Button(window, text="Enter IP Address", command=lambda: [get_ip_address()], bg='white', fg='black', font=('Arial', 10, 'bold'), width=20)
+ip_address_button = tk.Button(window, text="Enter", command=lambda: [get_ip_address()], bg='white',
+                              fg='black', font=('Arial', 10, 'bold'), width=20)
 ip_address_button.pack()
 ip_address_button.place(x=430, y=70)
 
-submit_button = tk.Button(window, text="Submit", command=submit, bg='white', fg='black', font=('Arial', 10, 'bold'), width=20)
+submit_button = tk.Button(window, text="Submit", command=submit, bg='white', fg='black', font=('Arial', 10, 'bold'),
+                          width=20)
 submit_button.pack()
 submit_button.place(x=100, y=110)
 
-instructions_button = tk.Button(window, text="Instructions", command=show_instructions, bg='white', fg='black', font=('Arial', 10, 'bold'), width=20)
+instructions_button = tk.Button(window, text="Instructions", command=show_instructions, bg='white', fg='black',
+                                font=('Arial', 10, 'bold'), width=20)
 instructions_button.pack()
 instructions_button.place(x=430, y=110)
-
-activity_icon_label = tk.Label(window, bg='white', image='', width=100, height=50)
-activity_icon_label.pack()
-activity_icon_label.place(x=100, y=150)
-
-
 
 window.geometry("1000x600")
 window.mainloop()
