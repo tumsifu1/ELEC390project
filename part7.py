5e0e042f08046005e6784b372921a6adca8a18c1
MPatchell <79283868+MPatchell@users.noreply.github.com>
2023-04-11 23:03:24 -0400

    Changed second plot and error fix

--- part7.py
+++ part7.py
@@ -8,13 +8,14 @@ from tkinter import filedialog
 import matplotlib.pyplot as plt
 import os
 
-#global varaibles
-filePath =""
+# global variables
+filePath = ""
 fileName = ""
 output_data = None
 Y_clf_prob_combined = None
 
-#feature extraction function called in the process data function
+
+# feature extraction function called in the process data function
 def data_feature_extraction(windows):
     w_size = 5
 
@@ -69,35 +70,35 @@ def data_feature_extraction(windows):
     all_features = np.concatenate((x_feature, y_feature, z_feature, total_feature), axis=0)
 
     labels = np.concatenate((np.ones((x_feature.shape[0], 1)),
-                         2 * np.ones((y_feature.shape[0], 1)),
-                         3 * np.ones((z_feature.shape[0], 1)),
-                         4 * np.ones((total_feature.shape[0], 1))), axis=0)
+                             2 * np.ones((y_feature.shape[0], 1)),
+                             3 * np.ones((z_feature.shape[0], 1)),
+                             4 * np.ones((total_feature.shape[0], 1))), axis=0)
 
     all_features = np.hstack((all_features, labels))
 
     return all_features
 
-#gets the name of the file in the input field
+
+# gets the name of the file in the input field
 def getname():
     global fileName
     fileName = inputtxt.get("1.0", "end-1c")
 
-#clears the input and output field
+
+# clears the input and output field
 def clear_selection():
     inputtxt.delete("1.0", "end")
     selected_file_label.config(text="")
 
-#allows you to plot scatter of the outputs
+
+# allows you to plot scatter of the outputs
 def view_plots(output_data):
     global Y_clf_prob_combined
-    # Convert indices in 'activity' column to 'walking' or 'jumping'
-    output_data['activity'] = np.where(output_data['activity'] == 0, 'walking', 'jumping')
 
     # Define x-axis data (window number)
     x_data = output_data.index
 
-    #get probabilities
-    
+    # get probabilities
 
     # Define y-axis data (walking and jumping probabilities)
     y_data = Y_clf_prob_combined.reshape((-1, 2))
@@ -108,7 +109,7 @@ def view_plots(output_data):
     # Create plot
     fig, ax = plt.subplots(figsize=(12, 6))
     for i in range(len(bar_labels)):
-        ax.bar(x_data + i*0.4, y_data[:, i], width=0.4, label=bar_labels[i])
+        ax.bar(x_data + i * 0.4, y_data[:, i], width=0.4, label=bar_labels[i])
 
     # Add activity labels above bars
     for i in range(len(x_data)):
@@ -118,13 +119,13 @@ def view_plots(output_data):
         else:
             label = 'J'
             color = 'orange'
-        ax.text(x_data[i] - 0.1, max(y_data[i])+0.05, label, color=color, fontweight='bold', fontsize=12)
+        ax.text(x_data[i] - 0.1, max(y_data[i]) + 0.05, label, color=color, fontweight='bold', fontsize=12)
 
     # Set plot labels and legend
     ax.set_xlabel('Window Number')
     ax.set_ylabel('Probability')
     ax.set_xticks(x_data)
-    ax.set_xticklabels(x_data+1)
+    ax.set_xticklabels(x_data + 1)
     ax.set_title('Walking and Jumping Probabilities by Window Number')
     ax.legend()
     ax.set_ylim([0, 1.2])
@@ -132,33 +133,33 @@ def view_plots(output_data):
     # Show plot
     plt.show()
 
-
     if output_data is None:
         tk.messagebox.showwarning(title="Warning", message="Output data is empty. Please try again!")
     output_data = output_data.drop(index=0)
 
-    print(output_data)
 
     walking_data = output_data[output_data['activity'] == 'walking']
     jumping_data = output_data[output_data['activity'] == 'jumping']
 
-
-    plt.scatter(jumping_data['x_max_val'], jumping_data['x_min_val'], c = 'red' ,label='Jumping')
-    plt.scatter(walking_data['x_max_val'], walking_data['x_min_val'], c = 'blue',label='walking')
-    plt.xlabel('X MAX VAL')
-    plt.ylabel('X MIN VAL')
+    plt.scatter(jumping_data['total_var_val'], jumping_data['total_mean_val'], c='blue', label='Jumping')
+    plt.scatter(walking_data['total_var_val'], walking_data['total_mean_val'], c='red', label='walking')
+    plt.xlabel('VAR')
+    plt.ylabel('MEAN')
+    plt.title('Scatter plot of mean vs range for the absolute acceleration of classified windows')
     plt.legend()
     plt.show()
 
-#gets the path for the outputs
+
+# gets the path for the outputs
 def getpath():
     global filePath
     global output_data
 
     filePath = filedialog.askopenfilename()
     selected_file_label.config(text=os.path.basename(filePath))
-    
-#process input the data and classifes it 
+
+
+# process input the data and classifes it
 def processData():
     global output_data
     global Y_clf_prob_combined
@@ -169,7 +170,7 @@ def processData():
     if not fileName:
         tk.messagebox.showwarning(title="Warning", message="Please enter a name for the output file before proceeding!")
         return
-    
+
     input_data = pd.read_csv(filePath)
     data = input_data.drop(columns=['Time (s)'])
 
@@ -189,8 +190,8 @@ def processData():
     data_features = data_feature_extraction(data_array)
 
     column_labels = np.array(
-    ['max_val', 'min_val', 'range_val', 'mean_val', 'median_val', 'var_val', 'skew_val', 'rms_val', 'kurt_val',
-    'std_val', 'measurement'])
+        ['max_val', 'min_val', 'range_val', 'mean_val', 'median_val', 'var_val', 'skew_val', 'rms_val', 'kurt_val',
+         'std_val', 'measurement'])
 
     dataset = pd.DataFrame(data_features, columns=column_labels)
 
@@ -220,12 +221,12 @@ def processData():
     Y_clf_prob_combined = clfCombined.predict_proba(X_combined)
 
     column_labels = np.array(
-    ['activity', 'x_max_val', 'x_min_val', 'x_range_val', 'x_mean_val', 'x_median_val', 'x_var_val', 'x_skew_val',
-    'x_rms_val', 'x_kurt_val', 'x_std_val', 'y_max_val', 'y_min_val', 'y_range_val', 'y_mean_val', 'y_median_val',
-    'y_var_val', 'y_skew_val', 'y_rms_val', 'y_kurt_val', 'y_std_val', 'z_max_val', 'z_min_val', 'z_range_val',
-    'z_mean_val', 'z_median_val', 'z_var_val', 'z_skew_val', 'z_rms_val', 'z_kurt_val', 'z_std_val',
-    'total_max_val', 'total_min_val', 'total_range_val', 'total_mean_val', 'total_median_val', 'total_var_val',
-    'total_skew_val', 'total_rms_val', 'x_kurt_val', 'x_std_val'])
+        ['activity', 'x_max_val', 'x_min_val', 'x_range_val', 'x_mean_val', 'x_median_val', 'x_var_val', 'x_skew_val',
+         'x_rms_val', 'x_kurt_val', 'x_std_val', 'y_max_val', 'y_min_val', 'y_range_val', 'y_mean_val', 'y_median_val',
+         'y_var_val', 'y_skew_val', 'y_rms_val', 'y_kurt_val', 'y_std_val', 'z_max_val', 'z_min_val', 'z_range_val',
+         'z_mean_val', 'z_median_val', 'z_var_val', 'z_skew_val', 'z_rms_val', 'z_kurt_val', 'z_std_val',
+         'total_max_val', 'total_min_val', 'total_range_val', 'total_mean_val', 'total_median_val', 'total_var_val',
+         'total_skew_val', 'total_rms_val', 'x_kurt_val', 'x_std_val'])
 
     output_data = pd.DataFrame(np.hstack((Y_output, X_combined)), columns=column_labels)
 
@@ -236,12 +237,52 @@ def processData():
     view_plots_btn.config(state=tk.NORMAL)  # Enable the view_plots_btn button
     return output_data
 
-# UI 
+
+# UI
 
 window = tk.Tk()
 
 # Set window background color to black
 window.configure(bg='black')
+
+window.title('Acceleration Classifier')
+
+greeting = tk.Label(text="Welcome to the acceleration classifier!", fg='#7FDBFF', bg='black',
+                    font=('Arial', 14, 'bold'), width=300, height=10)
+greeting.pack()
+
+btn = tk.Button(window, text="Select File", command=getpath, bg='white', fg='black', font=('Arial', 10, 'bold'),
+                width=20)
+btn.place(x=430, y=150)
+
+inputGreeting = tk.Label(text="Name your output file!", fg='#7FDBFF', bg='black', font=('Arial', 12))
+inputGreeting.pack()
+inputGreeting.place(x=425, y=200)
+
+inputtxt = tk.Text(window, height=1, width=21, bg='white', fg='black')
+inputtxt.pack()
+inputtxt.place(x=430, y=240)
+submit = tk.Button(window, text="Submit", command=lambda: [getname(), processData()], bg='white', fg='black',
+                   font=('Arial', 10, 'bold'), width=20)
+
+clear_selection_btn = tk.Button(window, text="Clear Selection", command=clear_selection, bg='white', fg='black',
+                                font=('Arial', 10, 'bold'), width=20)
+clear_selection_btn.place(x=430, y=350)
+
+progress_bar = ttk.Progressbar(window, orient="horizontal", length=300, mode="determinate")
+progress_bar.place(x=350, y=450)
+
+view_plots_btn = tk.Button(window, text="View Plots", command=lambda: view_plots(output_data), bg='white', fg='black',
+                           font=('Arial', 10, 'bold'), width=20, state=tk.DISABLED)
+view_plots_btn.place(x=430, y=400)
+
+submit.place(x=430, y=300)
+window.geometry("1000x600")
+
+selected_file_label = tk.Label(window, text="", fg='#7FDBFF', bg='black')
+selected_file_label.place(x=430, y=180)
+
+window.mainloop()
 
 window.title('Acceleration Classifier')
 
@@ -276,7 +317,3 @@ selected_file_label = tk.Label(window, text="", fg='#7FDBFF', bg='black')
 selected_file_label.place(x=430, y=180)
 
 window.mainloop()
-
-
-
-
